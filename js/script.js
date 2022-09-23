e76af12dd39470e4845a145c0aa8d46c9833b9dd
Ihor <ihor.burlaka511@gmail.com>
2022-09-23 21:01:15 +0300

    third comm

--- js/script.js
+++ js/script.js
@@ -60,3 +60,85 @@ switch (num) {
         break;        
 }
 
+//---------------------
+
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log('Not hungry!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'nonamelabel');
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Not hungry!');
+// } else {
+//     console.log('Get out from here!');
+// }
+
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('So glad!');
+} else {
+    console.log('Get out from here!');
+}
+
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+
+
+
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+
+
+console.log(!0);
+
+
+а
+
+
+console.log(NaN || 2 || undefined);
+console.log(NaN && 2 && undefined);
+console.log(1 && 2 && 3);
+console.log(!1 && 2 || !3);
+console.log(25 || null && !3);
+console.log(NaN || null || !3 || undefined || 5);
+console.log(NaN || null && !3 && undefined ||5);
+console.log(5 === 5 && 3 > 1 || 5);
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!')
+} else {
+    console.log('Not today!')
+}
+
+
+
