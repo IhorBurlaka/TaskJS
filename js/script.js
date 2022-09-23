aca57b1528d93237bdc373db4e42b9f20b713b79
Ihor <ihor.burlaka511@gmail.com>
2022-09-23 18:47:53 +0300

    Second comm

--- js/script.js
+++ js/script.js
@@ -1,26 +1,62 @@
 "use strick";
 
 
-const numberOfFilms = +prompt("How match films did you see?", "");
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const numberOfFilms = +prompt("How match films did you see?", "");
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt("You last film?", ""),
-      b = prompt('Your grade of this film?', ''),
-      c = prompt("You last film?", ""),
-      d = prompt('Your grade of this film?', '');
+// const a = prompt("You last film?", ""),
+//       b = prompt('Your grade of this film?', ''),
+//       c = prompt("You last film?", ""),
+//       d = prompt('Your grade of this film?', '');
       
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 
 
+//------------------------------
+
+if (4 == 9) {
+    console.log('OK!');
+} else {
+    console.log('Error');
+}
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too much');
+// } else {
+//     console.log('OK!');
+// }
+
+// (num === 50) ? console.log('OK!') : console.log('Error!');
+
+
+const num = 50;
+switch (num) {
+    case 49:
+        console.log('Not correctly');
+        break;
+    case 100:
+        console.log('Not correctly');
+        break;
+    case 50:
+        console.log('Correctly');
+        break;
+    default:
+        console.log('Not today');
+        break;        
+}
 
